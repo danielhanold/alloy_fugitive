4203ea3cf808a6e0b37362c26e672d354ef7037b
Daniel Hanold <danny@danielhanold.com>
2014-02-17 18:18:14 -0500

    Seed fugitives and display in Fugitives and Captured table views

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,44 @@
+// Determine if the database has already been seeded.
+// Only seed the database once.
+var seeded = Ti.App.Properties.hasProperty('seeded');
+Ti.API.info('seeded: ' + seeded);
+
+// Access the existing fugitives collection.
+var fugitives = Alloy.Collections.fugitives;
+fugitives.fetch();
+
+// Show the contents of each collections.
+Ti.API.info(JSON.stringify(fugitives.toJSON()));
+fugitives.each(function(fugitive, index) {
+  Ti.API.info('fugitive #' + index + ': ' + fugitive.get('name'));
+
+});
+
+
+
+if (!seeded) {
+  // Define yuppers names.
+	var names = ["Jeff Haynie", "Nolan Wright", "Blain Hamon", "Aaron Saunders", "Anthony Decena"];
+
+	// Loop through the names array to create a model
+  // representing each and save it to the collection.
+  _.each(names, function(name) {
+    var fugitive = Alloy.createModel('fugitives', {
+      name: name
+    });
+    Ti.API.info('Add fugitive: ' + name);
+
+    // Add this fugitive to the collection.
+    fugitives.add(fugitive);
+
+    // Save this fugitive in the database.
+    fugitive.save();
+  });
+
+	// set our app property so this code doesn't run next time.
+  Ti.App.Properties.setString('seeded', 'yuppers');
+}
+
+
+
 $.index.open();
