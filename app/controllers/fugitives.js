83ff819c45207fd7513a39ffe9abd6d4033e965c
Daniel Hanold <danny@danielhanold.com>
2014-02-22 18:37:58 -0500

    [ios] Add button to allow users to add a new fugitive

--- app/controllers/fugitives.js
+++ app/controllers/fugitives.js
@@ -3,3 +3,8 @@ var args = arguments[0] || {};
 // Access fugitives collection.
 var fugitives = Alloy.Collections.fugitives;
 fugitives.fetch();
+
+// Functions.
+function addNew() {
+  alert('Add New fugitive');
+};
